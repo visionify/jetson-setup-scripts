8dd6d050d719b60daa8016759f70fbda3509f6cf
Harsh Murari <hmurari@visionify.ai>
2021-12-10 15:13:12 -0700

    Added support for installing jetson-inference

--- jetson-setup.sh
+++ jetson-setup.sh
@@ -10,6 +10,7 @@ echo " ---> ONNX Runtime"
 echo " ---> PyTorch"
 echo " ---> Azure IoTEdge"
 echo " ---> Scikit-Learn (not working)"
+echo " ---> Jetson Inference library"
 
 source l4t-jetpack-version.sh > /dev/null
 echo " Your device: L4T_VERSION: R$L4T_VERSION, JetPack: $JP_VERSION"
@@ -88,6 +89,17 @@ while true; do
     esac
 done
 
+INSTALL_JETSON_INFERENCE=0
+while true; do
+    read -p "Do you wish to to install Jetson Inference (Hello AI World) (Y/N)? " yn
+    case $yn in
+        [Yy]* ) INSTALL_JETSON_INFERENCE=1; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
 if [ $INSTALL_TENSORFLOW != 0 ]; then
     if [ $JP_VERSION = "4.6" ]; then
         TENSORFLOW_URL="https://developer.download.nvidia.com/compute/redist/jp/v46/"
@@ -118,6 +130,7 @@ echo " Install Tensorflow (takes 30 mins)   : $INSTALL_TENSORFLOW"
 echo " Install ONNXRuntime                  : $INSTALL_ONNX_RUNTIME"
 echo " Install PyTorch                      : $INSTALL_PYTORCH"
 echo " Install scikit-learn (takes 30 mins) : $INSTALL_SCIKIT_LEARN"
+echo " Install jetson-inference             : $INSTALL_JETSON_INFERENCE"
 echo " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 
 while true; do
@@ -129,7 +142,7 @@ while true; do
     esac
 done
 
-if [ $INSTALL_IOTEDGE = 0 ] && [ $CONFIGURE_IOTEDGE = 0 ] && [ $INSTALL_TENSORFLOW = 0 ] && [ $INSTALL_ONNX_RUNTIME = 0 ] && [ $INSTALL_PYTORCH = 0 ] && [ $INSTALL_SCIKIT_LEARN = 0 ]; then
+if [ $INSTALL_IOTEDGE = 0 ] && [ $CONFIGURE_IOTEDGE = 0 ] && [ $INSTALL_TENSORFLOW = 0 ] && [ $INSTALL_ONNX_RUNTIME = 0 ] && [ $INSTALL_PYTORCH = 0 ] && [ $INSTALL_SCIKIT_LEARN = 0 ] && [ $INSTALL_JETSON_INFERENCE = 0 ]; then
     echo "No packages selected. Skipping install."
     exit
 else
@@ -167,11 +180,18 @@ fi
 
 if [ $INSTALL_TENSORFLOW != 0 ]; then
     echo " Install Tensorflow. (Be patient.. this takes ~30 mins)"
+    echo " .... Install dependencies: hdf5, zlib, zip, jpeg, liblapack, libblas, gfortran"
     apt-get -y install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran -qq > /dev/null
+    echo " .... Install dependencies: pip, testresources, setuptools"
     pip3 install -U pip testresources setuptools==49.6.0 -q > /dev/null
+    echo " .... Install dependencies: numpy 1.19.4, future, mock, keras dependencies, gast, protobuf, cython, pkgconfig"
     pip3 install -U --no-deps numpy==1.19.4 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 protobuf pybind11 cython pkgconfig -q > /dev/null
+    echo " .... Install dependencies: h5py"
     H5PY_SETUP_REQUIRES=0 pip3 install -U --no-build-isolation h5py==3.1.0 -q > /dev/null
-    pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
+    echo " .... Install Tensorflow"
+    pip3 install --pre --extra-index-url $TENSORFLOW_URL tensorflow -q > /dev/null
+    echo " .... Install keras"
+    pip3 install -U keras -q > /dev/null
 fi
 
 # echo " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
@@ -213,7 +233,7 @@ function changedir() {
 }
 
 if [ $INSTALL_SCIKIT_LEARN != 0 ]; then
-    echo " Install scikit-learn (Takes 30 mins) (Details: https://forums.developer.nvidia.com/t/how-to-install-numpy-scikit-image-scikit-learn-on-jetson-tx2/83819/7)"
+    echo " Install scikit-learn (Takes 30 mins) (Not working fully.) (Details: https://forums.developer.nvidia.com/t/how-to-install-numpy-scikit-image-scikit-learn-on-jetson-tx2/83819/7)"
     apt-get -y install liblapack-dev gfortran -qq > /dev/null
     wget -nc -q https://github.com/scipy/scipy/releases/download/v1.3.3/scipy-1.3.3.tar.gz
     tar -xzvf scipy-1.3.3.tar.gz scipy-1.3.3
@@ -236,6 +256,25 @@ if [ $INSTALL_SCIKIT_LEARN != 0 ]; then
     pip3 -U install scikit-image
 fi
 
+
+if [ $INSTALL_JETSON_INFERENCE != 0 ]; then
+
+    if [ -d "jetson-inference" ]; then
+        echo "jetson-inference/ directory already exists. Skipping install."
+
+    else
+        git clone --recursive https://github.com/dusty-nv/jetson-inference
+        cd jetson-inference
+        mkdir build
+        cd build
+        cmake ../
+
+        make -j4
+        make install
+        ldconfig
+
+    fi
+fi
 
 
 
